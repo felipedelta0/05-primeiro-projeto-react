cdc51083c415d7c2ea66f05630a3146a407830a0
Luis Felipe <felipedelta0@gmail.com>
2020-10-06 22:20:01 -0300

    Lidando com erros

--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -1,6 +1,10 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
 import { shade } from 'polished';
+
+interface FormProps {
+  hasError: boolean;
+}
 
 export const Title = styled.h1`
   font-size: 48px;
@@ -11,7 +15,7 @@ export const Title = styled.h1`
   margin-top: 80px;
 `;
 
-export const Form = styled.form`
+export const Form = styled.form<FormProps>`
   margin-top: 40px;
   max-width: 700px;
 
@@ -24,6 +28,11 @@ export const Form = styled.form`
     border: 0;
     border-radius: 5px 0 0 5px;
     color: #3A3A3A;
+    border: 2px solid #FFF;
+
+    ${(props) => props.hasError && css`
+      border-color: #C53030;
+    `}
 
     &::placeholder {
       color: #A8A8B3;
@@ -44,6 +53,12 @@ export const Form = styled.form`
       background: ${shade(0.2, '#04D361')}
     }
   }
+`;
+
+export const Error = styled.span`
+  display: block;
+  color: #C53030;
+  margin-top: 8px;
 `;
 
 export const Repositories = styled.div`
